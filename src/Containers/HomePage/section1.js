407478d6d84d3588587769c4c92497bb222d7c64
klk1236 <klk@persistence.one>
2021-07-09 12:10:34 +0530

    added Terms&conditions and privacy policy

--- src/Containers/HomePage/section1.js
+++ src/Containers/HomePage/section1.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactGa from 'react-ga';
 import Icon from "../../components/Icon";
-import pStake from "../../assets/pstake.mp4";
 const clickOnTwitter = () => {
     ReactGa.event({
         category: 'Twitter',
@@ -29,11 +28,11 @@ const sectionOne = () => {
             <section className="banner-section aos-init aos-animate" data-image-src="assets/bg.svg">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6 col-sm-12 banner-main-section">
+                        <div className="col-md-12 col-sm-12 banner-main-section">
                             <h3 className="banner-heading ">Unlocking Liquidity of <span>Staked Assets</span></h3>
                             <p className="caption">Securely stake your assets to earn staking rewards and receive 1:1 pegged staked representative tokens which can be used in DeFi to generate additional yield</p>
                             <div className="banner-buttons">
-                                <a rel="noopener noreferrer" target="_blank" href="http://alpha.pstake.finance">
+                                <a rel="noopener noreferrer" target="_blank" href="https://app.pstake.finance/">
                                     <span className="button xprt">
 
                                       
@@ -47,7 +46,7 @@ const sectionOne = () => {
                                     <span className="hideone">Launch pSTAKE</span>
                                     <span className="hidetwo">Coming Soon!</span>
                                 </span> */}
-                                <a rel="noopener noreferrer" target="_blank" className="button white-paper" href="https://discord.com/invite/SaBKpjbnhH"><Icon viewClass="social_icon_imgg" icon="discord" />&nbsp;Join Discord</a>
+                                {/* <a rel="noopener noreferrer" target="_blank" className="button white-paper" href="https://discord.com/invite/SaBKpjbnhH"><Icon viewClass="social_icon_imgg" icon="discord" />&nbsp;Join Discord</a> */}
                             </div>
                             <div className="social-icons">
                                 <ul className="list-unstyled first-section-social-icons">
@@ -68,15 +67,7 @@ const sectionOne = () => {
                             </div>
 
                         </div>
-                        <div className="col-md-6 text-center aside-section">
-                            {/* <img src={heroimg} alt="pStake"/> */}
-                            <video className="banner-video" autoPlay="autoplay" preload="true" loop="loop"
-                                muted src={pStake}>
-                                <source type="video/webm" src={pStake} />
-                                <source type="video/mp4" src={pStake} />
-                                <source type="video/ogg" src={pStake} />
-                            </video>
-                        </div>
+                      
                     </div>
                 </div>
 
--- src/Containers/HomePage/stakeAssets.js
+++ src/Containers/HomePage/stakeAssets.js
@@ -9,44 +9,43 @@ const StakeAssets = () => {
                 <div className="container">
                     <div className="row">
                         <h3 class="section-title">Stake Your Assets Now</h3>
-                        <Card className="col-md-4 col-xs-6">
-                            <Card.Body>
-                                <div className="text-center">
-                                <img className="pt-4 pb-4" src={asset1} title="Asset" alt="Asset"/>
-                                <Card.Title>Cosmos Network</Card.Title>
+                        <p className="text-center mt-3"> Stake Assets using pSTAKE to receive pASSETS which can be used in DeFi to generate yield while
+                            you earn staking rewards on your staked Assets</p>
+                     
+                            <Card className="col-md-5 col-xs-6">
+                                <Card.Body>
+                                    <div className="text-center">
+                                        <img className="pt-4 pb-4" src={asset1} title="Asset" alt="Asset" />
+                                        <Card.Title>Cosmos Network</Card.Title>
+                                    </div>
+
+
+
+                                </Card.Body>
+                                <div className="apr">
+                                    <h1>9.32% <span>APR</span></h1>
+                                    <a href="http://alpha.pstake.finance" rel="noopener noreferrer" target="_blank"><h5>Launch pSTAKE</h5></a>
                                 </div>
-                               
-                                <Card.Text>
-                                Stake ATOM using pSTAKE to receive pATOM which can be used in DeFi to generate yield while you earn rewards on your staked ATOM
-                                </Card.Text>
-                               
-                            </Card.Body>
-                            <div className="apr">
-                                <h1>9.32% <span>APR</span></h1>
-                                <a href="http://alpha.pstake.finance"rel="noopener noreferrer" target="_blank"><h5>Launch pSTAKE</h5></a>
-                            </div>
-                             {/* <div className="apr apr2">
+                                {/* <div className="apr apr2">
                                 <h1>9.32%  <span>APR</span></h1>
                                 <h5>Coming Soon!</h5>
                             </div> */}
-                        </Card>
-                        <Card className="col-md-4  col-xs-6">
-                            <Card.Body>
-                                <div className="text-center">
-                                <img className="pt-4 pb-4" src={asset2} title="Asset" alt="Asset"/>
-                                <Card.Title>Persistence</Card.Title>
+                            </Card>
+                            <Card className="col-md-5 col-xs-6">
+                                <Card.Body>
+                                    <div className="text-center">
+                                        <img className="pt-4 pb-4" src={asset2} title="Asset" alt="Asset" />
+                                        <Card.Title>Persistence</Card.Title>
+                                    </div>
+
+
+                                </Card.Body>
+                                <div className="apr apr2">
+                                    <h1>30.62% <span>APR</span></h1>
+                                    <h5>Coming Soon!</h5>
                                 </div>
-                               
-                                <Card.Text>
-                                Stake XPRT using pSTAKE to receive pXPRT which can be used in DeFi to generate yield while you earn rewards on your staked XPRT
-                                </Card.Text>
-                               
-                            </Card.Body>
-                            <div className="apr apr2">
-                                <h1>30.62% <span>APR</span></h1>
-                                <h5>Coming Soon!</h5>
-                            </div>
-                        </Card>
+                            </Card>
+                       
                     </div>
                 </div>
             </section>
