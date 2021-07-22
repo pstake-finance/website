bf678fb428761ad01baf0b6dde9c2f55fb9361a2
klk1236 <klk@persistence.one>
2021-07-22 16:02:22 +0530

    updated new version UI

--- src/Containers/HomePage/section1.js
+++ src/Containers/HomePage/section1.js
@@ -1,80 +1,33 @@
 import React from 'react';
-import ReactGa from 'react-ga';
-import Icon from "../../components/Icon";
-const clickOnTwitter = () => {
-    ReactGa.event({
-        category: 'Twitter',
-        action: 'Clicked on Twitter'
-    })
+import rightarrow from '../../assets/right-arrow.svg';
+import InfoSection from "../../Containers/HomePage/InfoSection";
 
-}
-const clickOnTelegram = () => {
-    ReactGa.event({
-        category: 'Telegram',
-        action: 'Clicked on Telegram'
-    })
-
-}
-const clickOnMedium = () => {
-    ReactGa.event({
-        category: 'Medium',
-        action: 'Clicked on Medium'
-    })
-
-}
 const sectionOne = () => {
     return (
         <React.Fragment>
-            <section className="banner-section aos-init aos-animate" data-image-src="assets/bg.svg">
+            <section className="banner-section aos-init aos-animate">
+                <div className="background-gradient">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 col-sm-12 banner-main-section">
                             <h3 className="banner-heading "> Unlocking Liquidity for <span>Staked Assets</span></h3>
-                            <p className="caption">Securely stake your assets to earn staking rewards and receive 1:1 pegged staked representative tokens which can be used in DeFi to generate additional yield</p>
+                            <p className="caption">Securely stake your assets to earn staking rewards and receive 1:1 pegged staked <span>representative tokens which can be used in DeFi to generate additional yield</span></p>
                             <div className="banner-buttons">
                                 <a rel="noopener noreferrer" target="_blank" href="https://app.pstake.finance/">
                                     <span className="button xprt">
-
-                                      
-                                        <span className="hideone">Launch pSTAKE</span> 
-                                       
+                                        <span className="hideone">Get Started&nbsp;</span> 
+                                        <img src={rightarrow} alt="arrow"/>
                                     </span>
-
-
                                </a>
-                                {/* <span className="button xprt">
-                                    <span className="hideone">Launch pSTAKE</span>
-                                    <span className="hidetwo">Coming Soon!</span>
-                                </span> */}
-                                {/* <a rel="noopener noreferrer" target="_blank" className="button white-paper" href="https://discord.com/invite/SaBKpjbnhH"><Icon viewClass="social_icon_imgg" icon="discord" />&nbsp;Join Discord</a> */}
-                            </div>
-                            <div className="social-icons">
-                                <ul className="list-unstyled first-section-social-icons">
-                                    <a href="https://twitter.com/pStakeFinance" onClick={clickOnTwitter} rel="noopener noreferrer" target="_blank"
-                                        title="Twitter">
-                                        <li><Icon viewClass="social_icon_imgg" icon="twitter-logo" /></li>
-                                    </a>
-
-                                    <a href="https://t.me/PersistenceOneChat" onClick={clickOnTelegram} rel="noopener noreferrer" target="_blank"
-                                        title="Telegram">
-                                        <li><Icon viewClass="social_icon_imgg" icon="telegram-plane" /></li>
-                                    </a>
-                                    <a href="https://pstake.medium.com/" onClick={clickOnMedium} rel="noopener noreferrer" target="_blank"
-                                        title="Medium">
-                                        <Icon viewClass="social_icon_img" icon="medium-m" />
-                                    </a>
-                                    <a href="https://discord.com/invite/SaBKpjbnhH" onClick={clickOnMedium} rel="noopener noreferrer" target="_blank"
-                                        title="Discord">
-                                        <Icon viewClass="social_icon_img" icon="discord" />
-                                    </a>
-                                </ul>
-                            </div>
-
+                              </div>
+                        
                         </div>
                       
                     </div>
                 </div>
-
+                <InfoSection/>
+                </div>
+               
             </section>
         </React.Fragment >
     );
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import SectionOne from '../Containers/HomePage/section1';
 import SectionTwo from '../Containers/HomePage/section2';
-import InfoSection from "../Containers/HomePage/InfoSection";
-// import PStakeUses from "../Containers/HomePage/pStakeUses";
 import LeadingPOS from '../Containers/HomePage/LeadingPOS';
 import FeactureBlog from '../Containers/HomePage/feactureBlog';
 import StakeAssets from '../Containers/HomePage/stakeAssets';
 import Footer from "../Containers/HomePage/Footer";
-// import Contact from "../Containers/HomePage/contact";
 const homePage = () => {
         return (
             <React.Fragment>
@@ -15,12 +12,8 @@ const homePage = () => {
                     <SectionOne />
                     <LeadingPOS />
                     <StakeAssets />
-                    <InfoSection/>
-                   
                     <SectionTwo />
                     <FeactureBlog />
-                    {/* <PStakeUses/> */}
-                    {/* <Contact/> */}
                     <Footer/>
                 </div>
             </React.Fragment >
