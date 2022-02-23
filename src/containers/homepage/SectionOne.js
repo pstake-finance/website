fa862d372f6192b55c114f3b91232a8235b0e5f1
klk1236 <klk@persistence.one>
2022-02-23 15:44:46 +0530

    updated ui

--- src/containers/homepage/SectionOne.js
+++ src/containers/homepage/SectionOne.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React from "react";
 import rightarrow from '../../assets/right-arrow.svg';
 import { useTranslation } from "react-i18next";
 import InfoSection from "../homepage/InfoSection";
@@ -9,34 +8,11 @@ import {
     PSTAKE_APP_URL,
     
 } from '../../constants/config';
-import { getTVLURL, getstkXPRTLIQUIDITYURL, getstkATOMLIQUIDITYURL } from "../../constants/url";
 
 const SectionOne = () => {
     const { t } = useTranslation();
-  const [totalTVLData, setTotalTVLData] = useState('0.00');
-  const [totalstkXPRTData, settotalstkXPRTData] = useState('0.00');
-  const [totalstkATOMData, settotalstkATOMData] = useState('0.00');
-
-    useEffect(() => {
-        const url = getTVLURL()
-        const getstkXPRTurl = getstkXPRTLIQUIDITYURL()
-        const getstkATOMurl = getstkATOMLIQUIDITYURL()
-        axios.get(url).then((result) => {
-            const totaltvldata = result.data;
-            setTotalTVLData((totaltvldata.TVL/1000000).toFixed(2))
-
-        });
-        axios.get(getstkXPRTurl).then((result) => {
-            const totalstkXPRTData = result.data;
-            settotalstkXPRTData((totalstkXPRTData.stkXPRTLiquidity/1000000).toFixed(2))
-
-        });
-        axios.get(getstkATOMurl).then((result) => {
-            const totalstkATOMData = result.data;
-            settotalstkATOMData((totalstkATOMData.stkATOMLiquidity/1000000).toFixed(2))
-
-        });
-    }, []);
+  
+   
 
     return (
         <React.Fragment>
@@ -47,13 +23,7 @@ const SectionOne = () => {
                             <div className="col-md-12 col-sm-12 banner-main-section">
                                 <h1 className="banner-heading mb-3">{t("UNLOCKING_LIQUIDITY")}<span> {t("STAKED_ASSETS")}</span></h1>
                                 <div className="banner-buttons">
-                                {/* <a rel="noopener noreferrer" target="_blank" href={VIDEO_URL}>
-                                        <span className="button xprt video">
-                                        <Icon viewClass="social_icon_img" icon="play" />&emsp;
-                                            <span className="hideone">{t("INTRO_VIDEO")}&nbsp;</span>
-                                           
-                                        </span>
-                                    </a> */}
+                               
                                     <h6>Stake your PoS assets while earning DeFi yields</h6>
                                     <a rel="noopener noreferrer" target="_blank" href={PSTAKE_APP_URL}>
                                         <span className="button xprt">
@@ -65,21 +35,18 @@ const SectionOne = () => {
                                 </div>
                                 <div className="network-stats">
                                     <div className="stats-count">
-                                        <h1>${totalTVLData}M+</h1>
-                                        <h6>{t("TOTAL_VALUE_LOCKED")}</h6>
+                                        <h1>$49.05M+</h1>
+                                        <h6>{t("Value of stkASSETs")}</h6>
                                     </div>
                                     <div className="stats-count">
-                                        <h1>02</h1>
-                                        <h6>{t("SUPPORTED_NETWORKS")}</h6>
+                                        <h1>$28.12M+ </h1>
+                                        <h6>{t("Liquidity of stkASSETs")}</h6>
                                     </div>
                                     <div className="stats-count">
-                                        <h1>${totalstkATOMData}M+</h1>
-                                        <h6>{t("SUSHI_POOL_LP_STKATOM_ETH")}</h6>
+                                        <h1>5,107</h1>
+                                        <h6>{t("Number of stakers")}</h6>
                                     </div>
-                                    <div className="stats-count">
-                                        <h1>${totalstkXPRTData}M+</h1>
-                                        <h6>{t("SUSHI_POOL_LP_STKXPRT_ETH")}</h6>
-                                    </div>
+                                   
 
                                 </div>
                             </div>
