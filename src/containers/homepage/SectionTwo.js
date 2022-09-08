1b6ab6a342f302a77af1078f0d8f41b783863b83
blackpanther1881 <raju@persistence.one>
2022-09-08 10:37:14 +0400

    updating homepage ui

--- src/containers/homepage/SectionTwo.js
+++ src/containers/homepage/SectionTwo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import feacturegradient2 from '../../assets/feacture-gradent2.svg';
-import usecase1 from "../../assets/usecase/usecase1.svg";
-import usecase2 from "../../assets/usecase/usecase2.svg";
-import usecase3 from "../../assets/usecase/usecase3.svg";
+import liquidity from "../../assets/liquidity.svg";
+import redemption from "../../assets/redemption.svg";
+import cash from "../../assets/cash.svg";
 
 const SectionTwo = () => {
     return (
@@ -15,7 +15,7 @@ const SectionTwo = () => {
                         <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
                             <div className="homecard feactures-card">
                                 <div className="icon-box">
-                                    <img className="pt-4 pb-4" src={usecase1} title="Asset" alt="Asset"/>
+                                    <img className="pt-4 pb-4" src={liquidity} title="Asset" alt="Asset"/>
                                 </div>
                                 <h5>Say goodbye to illiquidity</h5>
                                 <p>Receive stkAssets to maintain liquidity of otherwise locked PoS Asset when you stake</p>
@@ -24,7 +24,7 @@ const SectionTwo = () => {
                         <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
                             <div className="homecard feactures-card">
                                 <div className="icon-box">
-                                    <img className="pt-4 pb-4" src={usecase2} title="Asset" alt="Asset"/>
+                                    <img className="pt-4 pb-4" src={cash} title="Asset" alt="Asset"/>
                                 </div>
                                 <h5>Maximise your capital efficiency</h5>
                                 <p>Generate the highest yields with staking rewards & various stkAsset DeFi opportunities, all while securing the underlying PoS chain</p>
@@ -34,7 +34,7 @@ const SectionTwo = () => {
                             <img src={feacturegradient2} className="feacturegradient2" alt="feacture" />
                             <div className="homecard feactures-card">
                                 <div className="icon-box">
-                                    <img className="pt-4 pb-4" src={usecase3} title="Asset" alt="Asset"/>
+                                    <img className="pt-4 pb-4" src={redemption} title="Asset" alt="Asset"/>
                                 </div>
                                 <h5>Redemption time?</h5>
                                 <p>Skip unstaking and unbonding process of your Pos Asset by directly swapping stkAsset from a liquidity pool</p>
