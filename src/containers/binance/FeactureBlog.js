5fc2efe5e1be863cca5124c7dccb15d3d15951a2
blackpanther1881 <raju@persistence.one>
2022-09-14 12:59:40 +0400

    wombat integration

--- src/containers/binance/FeactureBlog.js
+++ src/containers/binance/FeactureBlog.js
@@ -6,17 +6,15 @@ import {
     PSTAKE_BLOG_TWO_URL,
     PSTAKE_BLOG_TWO_IMG_URL,
     PSTAKE_BLOG_THREE_URL,
-    PSTAKE_BLOG_THREE_IMG_URL,
+    PSTAKE_BLOG_THREE_IMG_URL, PSTAKE_MEDIUM_URL,
 } from '../../constants/config';
 import Icon from "../../components/Icon";
 const FeaturedBlogs = () => {
     const { t } = useTranslation();
     return (
         <React.Fragment>
-            <section className="stay-updated blog-section">
-                <div className="title-line-section">
-                    <h3 className="section-title">{t("FEACTURED_BLOGS")}</h3>
-                </div>
+            <section className="blogs-section">
+                <h3 className="section-title">{t("FEATURED_BLOGS")}</h3>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4 blog-articles left">
@@ -29,31 +27,28 @@ const FeaturedBlogs = () => {
                                                 <img className="card-img-top" alt="blog2"
                                                      src={PSTAKE_BLOG_ONE_IMG_URL} />
                                             </div>
-
                                             <div className="custom-card-body">
-                                                <p className="text1">{t("USER GUIDE")}</p>
+                                                <p className="tag">{t("The State of ATOM Staking")}</p>
                                                 <p className="learn-more">{t("LEARN_MORE")}<Icon viewClass="arrow-right" icon="arrow-right" /></p>
                                             </div>
                                         </div>
                                     </a>
-
                                 </div>
-
                             </div>
                         </div>
                         <div className="col-md-4 blog-articles left">
                             <div className="row m-0">
                                 <div className="col-md-12">
                                     <a href={PSTAKE_BLOG_TWO_URL}
-                                        target="_blank" rel="noopener noreferrer">
+                                       target="_blank" rel="noopener noreferrer">
                                         <div className="custom-card mb-10" data-aos="fade-up">
                                             <div className="blog-image">
                                                 <img className="card-img-top" alt="blog2"
-                                                    src={PSTAKE_BLOG_TWO_IMG_URL}/>
+                                                     src={PSTAKE_BLOG_TWO_IMG_URL}
+                                                />
                                             </div>
-
                                             <div className="custom-card-body">
-                                                <p className="text1">LIQUID STAKING</p>
+                                                <p className="tag">{t("Why Liquid Staking is a Game-Changer for BNB")}</p>
                                                 <p className="learn-more">{t("LEARN_MORE")}<Icon viewClass="arrow-right" icon="arrow-right" /></p>
                                             </div>
                                         </div>
@@ -74,7 +69,7 @@ const FeaturedBlogs = () => {
                                             </div>
 
                                             <div className="custom-card-body">
-                                                <p className="text1">STAKE ATOM</p>
+                                                <p className="tag">{t("stkBNB User Guide")}</p>
                                                 <p className="learn-more">{t("LEARN_MORE")}<Icon viewClass="arrow-right" icon="arrow-right" /></p>
                                             </div>
                                         </div>
@@ -82,13 +77,14 @@ const FeaturedBlogs = () => {
                                 </div>
                             </div>
                         </div>
-                        {/*<div className="banner-buttons">*/}
-                        {/*            <a rel="noopener noreferrer" target="_blank" href={PSTAKE_MEDIUM_URL}>*/}
-                        {/*                <span className="button xprt">*/}
-                        {/*                    <span className="hideone">{t("READ_ALL")}&nbsp;</span>*/}
-                        {/*                </span>*/}
-                        {/*            </a>*/}
-                        {/*        </div>*/}
+                        <div className="banner-buttons">
+                            <a rel="noopener noreferrer" target="_blank" href={PSTAKE_MEDIUM_URL}>
+                                <span className="button xprt">
+                                    <span className="hideone">{t("READ_ALL")}&nbsp;</span>
+                                </span>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </section>
