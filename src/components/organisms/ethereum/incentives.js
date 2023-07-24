f16c00909df96d238a0a5e4c66ad339ce61cfe5c
blackpanther1881 <raju@persistence.one>
2023-07-24 21:33:53 +0530

    update eth page

--- src/components/organisms/ethereum/incentives.js
+++ src/components/organisms/ethereum/incentives.js
@@ -14,16 +14,17 @@ const Incentives = () => {
           </p>
           <div className="rounded-md bg-[#26262B] p-6 mb-4">
             <p className={"text-light-high"}>
-              1. Win a share of $500 in $PSTAKE for the top 10 most valuable
-              feedback submissions.
+              1. <b>Beta testers:</b> Win a share of $500 in $PSTAKE for the top
+              10 most valuable feedback submissions.
             </p>
           </div>
           <div className="rounded-md bg-[#26262B] p-6">
             <p className={"text-light-high mb-4"}>
-              2. Claim the exclusive stkETH Marvel NFTs on Galxe which will
-              serve as a proof of your participation in the testnet, and unlocks
-              a rewarding opportunity to receive a share of $10,000 in $PSTAKE
-              rewards on Mainnet, based on the below-outlined criteria:
+              2. <b>Road to Mainnet:</b> Claim exclusive stkETH Marvel NFTs on
+              Galxe, proving testnet participation and unlocking a rewarding
+              opportunity to share a pool of $10,000 in $PSTAKE for staking a
+              minimum of 0.1 ETH on Mainnet, based on the below-outlined
+              criteria:
             </p>
             <table className="border-separate border-spacing-0 border border-[#DBD6D6] w-full rounded-md">
               <thead>
