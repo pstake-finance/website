1c1ae56681a3f968708da929b4ca1f21cbd8c433
blackpanther1881 <rj.vemula81@gmail.com>
2024-05-17 17:40:18 +0530

    add btc page

---
+++ src/pages/btc/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../components/molecules/Footer";
+import Banner from "../../components/organisms/btc/Banner";
+import ScrollToTop from "../../components/ScrollToTop";
+
+const Atom = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <Banner />
+      <Footer />
+    </>
+  );
+};
+
+export default Atom;
