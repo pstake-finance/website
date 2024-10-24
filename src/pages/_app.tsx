9f296a5ee8a2d9d1770396c6a3f625e3ed9b0005
rajuvemula <rj.vemula81@gmail.com>
2024-10-24 12:23:36 +0530

    fix tooltip styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import "aos/dist/aos.css";
 import Header from "../components/molecules/header";
 import * as gtag from "../utils/gtag";
 import "../styles/globals.css";
+import "rc-tooltip/assets/bootstrap.css";
 import "react-multi-carousel/lib/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
