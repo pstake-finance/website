63aa638cbb0fa7e36b4ee591ec02056c11e3d25a
saurabhfegade <saurabh.fegade1998@gmail.com>
2024-12-23 15:15:02 +0530

    change banner

--- src/components/molecules/geofence-banner/index.tsx
+++ src/components/molecules/geofence-banner/index.tsx
@@ -19,7 +19,7 @@ const GeofenceNotice = () => {
       <p className="text-[#1D1D22] text-[12px] font-semibold flex items-center flex-wrap text-center justify-center">
         <a
           className="link flex items-center"
-          href={"https://app.btc.pstake.finance/"}
+          href={"https://x.com/pStakeFinance/status/1870098381475127671"}
           target="_blank"
           rel="noopener noreferrer"
         >
